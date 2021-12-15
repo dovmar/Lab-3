d9e60bacbdc79dd91c18d409a051ce29401f6544
Dovydas Martinkus <dovydas.martinkus@gmail.com>
2021-12-15 21:22:05 +0200

    Clean up

--- Gataveckas_Gaulia_Martinkus_laboratorinio_3_kodas_R_DM_2gr.R
+++ Gataveckas_Gaulia_Martinkus_laboratorinio_3_kodas_R_DM_2gr.R
@@ -1,71 +1,106 @@
-## ---- include=FALSE---------------------------------------------------------------------------------------------------
-knitr::opts_chunk$set(warning=FALSE,message=FALSE)
+## ---- include=FALSE------------------------------------------------------------------------------------------
+knitr::opts_chunk$set(warning = FALSE, message = FALSE)
 
 
-## ---------------------------------------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------
 library(tidyverse)
 library(car)
 library(readxl)
 library(janitor)
 
-x<-readxl::read_xlsx("HighSchool.xlsx",sheet = 1) %>% clean_names()
+x <- readxl::read_xlsx("HighSchool.xlsx", sheet = 1) %>% clean_names()
 
 
-## ---------------------------------------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------
 x <- x %>%
+  # sudaromas jungtinis faktorius
+  mutate(combined = factor(paste(parental_education, test_prep_course))) %>%
   drop_na()
 
 
-# Duomenys išsaugomi į failą
-write_csv(x,"high_school_modified.csv")
+write_csv(x, "high_school_modified.csv")
 
 
-# Koeficiento lygybės skirtingiems faktoriaus lygmenims patikrinimas
-ggplot(x,aes(attendance,result,color=parental_education)) + geom_point() + geom_smooth(method="lm",se=FALSE) +
+# Tiriamieji grafikai
+
+# Faktorių efektai neatsižvelgiant į kovariantes
+ggplot(x, aes(parental_education, result, color = test_prep_course, group = test_prep_course)) +
+  stat_summary(fun = "mean", geom = "line", size = 1) +
+  theme_minimal(base_size = 16) +
+  guides(x = guide_axis(n.dodge = 2))
+
+
+# Hipotezės apie koeficientų lygybę visiems faktorių lygmenims
+ggplot(x, aes(attendance, result, color = parental_education)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(vars(test_prep_course)) +
   theme_minimal()
 
-ggplot(x,aes(daily_study_hours,result,color=parental_education)) + geom_point() + 
-  geom_smooth(method="lm",se=FALSE) + theme_minimal() + scale_color_brewer(palette="Set2")
+
+ggplot(x, aes(daily_study_hours, result, color = parental_education)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(vars(test_prep_course)) +
+  theme_minimal()
 
 
-ggplot(x,aes(x=parental_education,y=daily_study_hours,color=special_coaching)) + geom_boxplot() + theme_minimal() +
-scale_color_brewer(palette="Set2")
+# Kovariančių pasiskirstymas pagal faktorių lygmenis
+ggplot(x, aes(x = parental_education, y = result, color = special_coaching)) +
+  geom_boxplot() +
+  theme_minimal() +
+  scale_color_brewer(palette = "Set2")
 
 
-ggplot(x,aes(x=parental_education,y=attendance,color=special_coaching)) +
-  geom_boxplot() + theme_minimal() + scale_color_brewer(palette="Set2")
+ggplot(x, aes(x = parental_education, y = attendance, color = special_coaching)) +
+  geom_boxplot() +
+  theme_minimal() +
+  scale_color_brewer(palette = "Set2")
 
 
-## ---------------------------------------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------
 library(rstatix)
 
-anova_test(result~attendance*parental_education + daily_study_hours*parental_education,data=x,type=3, detailed=TRUE) # Hipotezės apie koeficientų lygybę visiems faktoriaus lygmenims neatmetos
+# Hipotezė apie koeficientų lygybę neatmetama
+anova_test(result ~ attendance * combined + daily_study_hours * combined, data = x, type = 3, detailed = TRUE)
 
 
-# Kovariacinės analizės modelio sukūrimas
-model <- anova_test(result~attendance + daily_study_hours + parental_education,data=x,type=3, detailed=TRUE)
+# Hipotezė apie faktorių sąveikos nebuvimą neatmetama
+anova_test(result ~ attendance + daily_study_hours + parental_education * test_prep_course, data = x, type = 3, detailed = TRUE)
+
+
+model <- anova_test(result ~ attendance + daily_study_hours + parental_education + test_prep_course, data = x, type = 3, detailed = TRUE)
 model
 
 
-## ---------------------------------------------------------------------------------------------------------------------
-model_lm <- lm(result~attendance + daily_study_hours + parental_education,data=x)
+## ------------------------------------------------------------------------------------------------------------
+# Modelio prielaidų patikrinimas
+model_aov <- aov(result ~ attendance + daily_study_hours + parental_education + test_prep_course, data = x)
 
-# Dispersijų lygybė grupėms
-leveneTest(result~parental_education,data=x)
-# Liekanų normalumas
-shapiro.test(resid(model_lm))
+plot(model_aov)
 
-plot(model_lm)
-# Išskirtys
-plot(cooks.distance(model_lm))
-plot(hatvalues(model_lm))
+leveneTest(result ~ combined, data = x, center = "mean")
+shapiro.test(resid(model_aov))
 
 
-## ---------------------------------------------------------------------------------------------------------------------
+# Tiesinis ryšys tarp kovariančių ir priklausomo kintamojo
+ggplot(x, aes(attendance, result, color = combined)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_minimal() +
+  facet_wrap(vars(combined))
+
+ggplot(x, aes(daily_study_hours, result, color = combined)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_minimal() +
+  facet_wrap(vars(combined))
+
+
+## ------------------------------------------------------------------------------------------------------------
 library(emmeans)
 
-means <- emmeans_test(result~parental_education,covariate = c(daily_study_hours,attendance),p.adjust.method="tukey",data=x) 
-means
-
-get_emmeans(means)
+res <- x %>% emmeans_test(result ~ parental_education, covariate = c(daily_study_hours, attendance), model = model_aov)
+res
+get_emmeans(res)
 
--- Gataveckas_Gaulia_Martinkus_laboratorinio_3_kodas_SAS_DM_2gr.sas
+++ Gataveckas_Gaulia_Martinkus_laboratorinio_3_kodas_SAS_DM_2gr.sas
@@ -6,9 +6,14 @@ RUN;
 
 
 /* Hipotezė apie krypties koeficientų lygybę*/
-PROC GLM DATA=data; CLASS parental_education;
-MODEL result = parental_education daily_study_hours attendance
-daily_study_hours*parental_education attendance*parental_education / SS3; 
+PROC GLM DATA=data; CLASS combined;
+MODEL result = combined daily_study_hours*combined attendance*combined / SS3; 
+RUN;
+
+
+/* Hipotezė apie faktorių sąveikos nebuvimą*/
+PROC GLM DATA=data; CLASS parental_education test_prep_course;
+MODEL result = parental_education test_prep_course parental_education*test_prep_course daily_study_hours attendance / SS3; 
 RUN;
 
 
@@ -16,10 +21,24 @@ RUN;
 /* Modelio prielaidos */
 /* Vidurkių palyginimai */
 PROC GLM DATA=data plots=ALL;
- CLASS parental_education;
-MODEL result = parental_education daily_study_hours attendance / SS3; 
+CLASS parental_education test_prep_course;
+MODEL result = parental_education test_prep_course daily_study_hours attendance / SS3; 
 LSMEANS parental_education / stderr pdiff adjust=bon;
+OUTPUT out=res residual=liekanos;
 RUN;
 
+
+/* Normalumo testas */
+PROC UNIVARIATE data=res normal;
+VAR liekanos;
+RUN;
+
+
+/* Dispersijų lygybėstestas */
+PROC GLM DATA=data plots=none;
+CLASS combined;
+MODEL result = combined;
+MEANS combined / HOVTEST=levene(type=abs);
+RUN;
 
 
